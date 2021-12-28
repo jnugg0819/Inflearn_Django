7d10cf4a29e28327a5d03abdf3df1a8b17f52868
Olio <jgg0819@naver.com>
2021-12-29 00:51:09 +0900

    장고 admin을 이용해서 admin 관리하기
    - Add field is_public in Post class
    - Customize and register admin page

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+@admin.register(Post)  # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'created_at',
+                    'message_length', 'is_public', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']  # message fields로 검색을 추가하겠다
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -5,5 +5,16 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # Java의 toString
+    def __str__(self):
+        return self.message
+
+    # # 메시지 글자숫자 함수로 리턴해주면 admin.py에 display_liks등록된거에 매핑되어 들어간다.
+    # def message_length(self):
+    #     return len(self.message)
+    # # MESSAGELENGTH라나오는데 별명이라고 지어줌.
+    # message_length.short_description = "메세지 글자수"
