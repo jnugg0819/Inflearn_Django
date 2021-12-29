6362039fe9abd00c81b1063dcf2dacab04706a1d
Olio <jgg0819@naver.com>
2021-12-30 00:28:14 +0900

    Media파일 다루기
    - Add mediaurl and root in settings.py
    - How to set  media folder path
    - Add ImageField in models
    - Add requirements file( set django at once )

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 
 @admin.register(Post)  # Wrapping
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message', 'created_at',
+    list_display = ['id', 'photo_tag', 'message', 'created_at',
                     'message_length', 'is_public', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at', 'is_public']
     search_fields = ['message']  # message fields로 검색을 추가하겠다
+
+    def photo_tag(self, post):
+        if(post.photo):
+            return mark_safe(f'<img src="{post.photo.url}" style="width: 72px;"/>')
+        return None
 
     def message_length(self, post):
         return f"{len(post.message)} 글자"
