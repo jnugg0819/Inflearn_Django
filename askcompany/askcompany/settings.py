ee575688a354176544bea31187ce5e27b8b75cad
Olio <jgg0819@naver.com>
2022-01-05 22:50:41 +0900

    Add Debug toolbar

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog1' ,
+    'debug_toolbar',
+    'django_extensions',
+    'blog1',
     'instagram',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = ['127.0.0.1']
