07e379e6a6271619e37ad49643ef923a634e6973
Olio <jgg0819@naver.com>
2022-01-11 23:29:28 +0900

    Create many to many field
    - Add Tag class
    - Add Tag admin
    - How to put array to param(unpack)

--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y%m%d')
+    tag_set = models.ManyToManyField('Tag', blank=True)  # 태그가 없는 상황도있으니까
     is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -34,8 +35,17 @@ class Comment(models.Model):
     # 여기서 Post라고 써도되고 'Post'라고 문자열처럼 써도되고 , 앞에 앱이름을 써도된다.('instagram/Post')
     # 이때는 다른앱의 모델을 쓰고싶을떄 쓴다.
     # post_id 라는 필드가 생성됩니다.
+    # is_public 은 공개여부가 공개로되어있는냐(true이냐)인것만 박스를 띄우겠다는뜻.
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, limit_choices_to={'is_public': True})
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    #post_set = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.name
